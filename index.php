d0cde83ae139a2f555c9f3b9e9d596966c86c3f8
Jiplusplus <jiweikai03@icloud.com>
2023-01-20 22:16:01 +0100

    upgrade

--- index.php
+++ index.php
@@ -4,18 +4,27 @@
         <title>Parchi</title>
     </head>
     <body>
-    <?php
-        function getRowsNumber() {
-        $sql = "SELECT COUNT(*) FROM users";
-        $stmt = $this->connect()->query($sql);
-        $count = $stmt->fetchColumn();
-        print $count;
-            }
-    ?>
+    <script>
+function showUser(str) {
+  if (str=="") {
+    document.getElementById("txtHint").innerHTML="";
+    return;
+  }
+  var xmlhttp=new XMLHttpRequest();
+  
+  xmlhttp.onreadystatechange=function() {
+    if (this.readyState==4 && this.status==200) {
+      document.getElementById("txtHint").innerHTML=this.responseText;
+    }
+  }
+  xmlhttp.open("GET","document.URL&specie=+"+str,true);
+  xmlhttp.send();
+}
+</script>
+
         <form method="GET" action="index.php">
             <select name="parco_id">
                 <?php
-
                 $ip = '127.0.0.1';
                 $username = 'root';
                 $pwd = '';
@@ -38,16 +47,14 @@
                         }
                     }
                 }
-
+            
                 ?>
             </select>
             <input type="submit" />
         </form>
-
-        <form method="GET" action="index.php">
-            <select name="specie_id">
+            <select name="specie_id" onchange=showUser(this.value)>
                 <?php
-
+                if (isset($_REQUEST['parco_id'])) {
                 $ip = '127.0.0.1';
                 $username = 'root';
                 $pwd = '';
@@ -58,51 +65,64 @@
                     die('C\'è stato un errore: ' . $connection->connect_error);
                 }
 
-                // echo 'Database collegato';
-                $sql = 'SELECT id_specie, nome, id_parco FROM specie';
+                echo '<option>Specie</option>';
+                $sql = 'SELECT DISTINCT s.nome FROM animale a INNER JOIN parco p ON a.id_parco = '.$_REQUEST['parco_id'].' INNER JOIN specie s ON s.id_specie = a.id_specie;';
                 $result = $connection->query($sql);
 
                 if ($result->num_rows > 0) {
                     while ($row = $result->fetch_assoc()) {
-                        echo '<option value="' . $row['id_specie'] . '">' . $row['nome'] . '</option>';
+                        echo '<option value="' . $row['nome'] . '">' . $row['nome'] . '</option>';
                     }
                 }
-
-                ?>
+                }
+            ?>
+                
             </select>
-            <input type="submit" />
         </form>
 
         <form method="GET" action="index.php">
             <select name="animale_id">
                 <?php
-
-                $ip = '127.0.0.1';
-                $username = 'root';
-                $pwd = '';
-                $database = 'parchi';
-                $connection = new mysqli($ip, $username, $pwd, $database);
-
-                if ($connection->connect_error) {
-                    die('C\'è stato un errore: ' . $connection->connect_error);
-                }
-
-                // echo 'Database collegato';
-                $sql = 'SELECT id_animale, data_nascita, sesso, stato_salute, id_specie, id_parco FROM animale';
-                $result = $connection->query($sql);
-
-                if ($result->num_rows > 0) {
-                    while ($row = $result->fetch_assoc()) {
-                        if($row['id_animale'] == $_GET['animale_id']){
-                            echo '<option selected value="' . $row['id_animale'] . '">' . $row['data_nascita'] . ' ' . $row['sesso'] . ' ' . $row['stato_salute'] .'</option>';
-                        }else{
-                            echo '<option selected value="' . $row['id_animale'] . '">' . $row['data_nascita'] . ' ' . $row['sesso'] . ' ' . $row['stato_salute'] .'</option>';
-                        }
+                $message=$_SERVER['QUERY_STRING'];
+                $nome_animale=null;
+                echo $message;
+                if(str_contains($message,'specie')==true){
+                   
+                    $nome_animale=explode('=',$message)[3];
+                     echo $nome_animale;
                     
+                }
+                if ( $nome_animale!=null){
+
+
+                    $ip = '127.0.0.1';
+                    $username = 'root';
+                    $pwd = '';
+                    $database = 'parchi';
+                    $connection = new mysqli($ip, $username, $pwd, $database);
+    
+                    if ($connection->connect_error) {
+                        die('C\'è stato un errore: ' . $connection->connect_error);
+                    }
+    
+                    // echo 'Database collegato';
+                    $sql = 'SELECT a.id_animale, a.data_nascita, a.sesso, a.stato_salute, s.nome FROM animale a INNER JOIN parco p ON a.id_parco = '.$_REQUEST['parco_id'].' INNER JOIN specie s ON s.id_specie = a.id_specie';
+                    $result = $connection->query($sql);
+    
+                    if ($result->num_rows > 0) {
+                        while ($row = $result->fetch_assoc()) {
+                            if($row['id_animale'] == $_GET['animale_id']){
+                                echo '<option selected value="' . $row['id_animale'] . '">' . $row['data_nascita'] . ' ' . $row['sesso'] . ' ' . $row['stato_salute'] .'</option>';
+                            }else{
+                                echo '<option selected value="' . $row['id_animale'] . '">' . $row['data_nascita'] . ' ' . $row['sesso'] . ' ' . $row['stato_salute'] .'</option>';
+                            }
+                        
+                        }
                     }
                 }
+               
                 
-
+            
                 ?>
             </select>
             <input type="submit" />
@@ -129,6 +149,6 @@
             }
         }
         ?>
-        
+        <div id="txtHint"></div>
     </body>
 </html>
